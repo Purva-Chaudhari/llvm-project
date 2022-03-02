87aefe70ddfe36bbbe113b678849080827779d38
Purva-Chaudhari <purva.chaudhari02@gmail.com>
2022-03-03 01:08:22 +0530

    test2

--- clang/test/Interpreter/recovery.cpp
+++ clang/test/Interpreter/recovery.cpp
@@ -1,2 +1,1 @@
-// RUN: clang-repl -recovery -Xcc -fsyntax-only -Xcc -verify %s/../Sema/address-constant.c
-// RUN: %clang_cc1 -fsyntax-only -verify %s/../Sema/address-packed-member-memops.c
+// RUN: clang-repl -Xcc -fsyntax-only -Xcc -verify %s/../Sema/address-constant.c
